9c01a00ec04d6f482a01c977a9fd56837bcf32e6
Thousandspring <66168401+Thousandsprings@users.noreply.github.com>
2022-04-22 09:25:22 +0900

    set a variable

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,32 @@
 function loaded() {
-  let codes = {
-    "000000000018": "05397-2000",
-    "000000003415": "66064",
+  const codes = {
+    // ブランデーケーキ掛け紙 英字柄(247×295mm)
+    // "000000000018": "05397-2000",
+    // その他付属品 カテゴリー
+    ct538: "7030",
   };
 
-  const oldValue = codes["000000000018"];
-  ("https://skweb.marubishi-group.co.jp/shopdetail/${oldValue}/index.html");
-  const newValue = oldValue;
-  document.getElementById("loading").classList.remove("active");
+  // const newValue = codes["000000000018"];
 
-  window.location.href = `https://orderie.jp/component/g/g${newValue}/`;
+  const oldValue = Object.keys(codes);
+
+  console.log(oldValue);
+
+  // SKカテゴリー＝ラッピング
+  // https://skweb.marubishi-group.co.jp/shopbrand/ct538/
+
+  if (a in codes) {
+    let newValue = codes[a[0]];
+
+    document.getElementById("loading").classList.remove("active");
+
+    //対応する商品↓
+    // window.location.href = `https://orderie.jp/component/g/g${newValue}/`;
+
+    // カテゴリーなら ↓
+    window.location.href = `https://orderie.jp/component/c/c${newValue}/`;
+  } else {
+    window.location.href = "https://orderie.jp/";
+  }
 }
 setTimeout(loaded, 3000);
