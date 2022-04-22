c805f5abacc49d62f8c78a64fc6136ebc6a06b8f
Thousandspring <66168401+Thousandsprings@users.noreply.github.com>
2022-04-22 23:16:10 +0900

    get a pathname and compare

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,35 @@
 function loaded() {
   const codes = {
-    // ブランデーケーキ掛け紙 英字柄(247×295mm)
-    // "000000000018": "05397-2000",
-    // その他付属品 カテゴリー
-    ct538: "7030",
+    // ブランデーケーキ掛け紙 英字柄(247×295mm) SK:その他ペーパー・シート, ODR:その他付属品 (カテゴリー分類)
+    "/shopbrand/ct560/": "/component/c/c7030",
+
+    //要配列追加
   };
 
-  // const newValue = codes["000000000018"];
+  // ブランデーケーキ掛け紙英字柄(247×295mm) SKサイト
+  //カテゴリーだとその他ペーパー・シート ↓
+  //https://skweb.marubishi-group.co.jp/shopbrand/ct560/
+  //商品詳細だと↓
+  // https://skweb.marubishi-group.co.jp/shopdetail/000000000018/ct560/page1/order/
+  // この場合はlocation.path=/shopdetail/000000000018/ct560/page1/order/ がlocation.pathname?
 
-  const oldValue = Object.keys(codes);
+  //get a current URL
+  // const currentUrl = location.href;
 
-  console.log(oldValue);
+  //get a pathname of SK
+  const pathName = location.pathname;
+  console.log(pathName);
+  // /shopbrand/ct560/l
 
-  // SKカテゴリー＝ラッピング
-  // https://skweb.marubishi-group.co.jp/shopbrand/ct538/
+  // const oldValue = Object.keys(codes);
+  const newValue = codes[pathName];
 
-  if (a in codes) {
-    let newValue = codes[a[0]];
+  document.getElementById("loading").classList.remove("active");
 
-    document.getElementById("loading").classList.remove("active");
-
-    //対応する商品↓
-    // window.location.href = `https://orderie.jp/component/g/g${newValue}/`;
-
-    // カテゴリーなら ↓
-    window.location.href = `https://orderie.jp/component/c/c${newValue}/`;
-  } else {
+  if (pathName == void 0) {
     window.location.href = "https://orderie.jp/";
+  } else {
+    window.location.href = `https://orderie.jp${newValue}`;
   }
 }
 setTimeout(loaded, 3000);
